068a77a7688d0901deafde0c9fb74c8287379886
suhassimon <suhassimon@gmail.com>
2022-11-11 20:19:10 +0530

    calc content

---
+++ calculator.py
@@ -0,0 +1,27 @@
+a= input("=>").replace(" ","")
+
+if '+' in a :
+    input_split = a.split("+")
+    ans= int(input_split[0]) + int(input_split[1]);
+elif '-' in a :
+    input_split = a.split("-")
+    ans = int(input_split[0]) - int(input_split[1]);
+elif '*' in a :
+    input_split = a.split("*")
+    ans = int(input_split[0]) * int(input_split[1]);
+elif '/' in a :
+    input_split = a.split("/")
+    ans = int(input_split[0]) / int(input_split[1]);
+else:
+    ans = "Please enter in this format: Digit1 Operator Digit 2"
+
+print(f'=> {ans}')
+
+
+
+
+
+
+
+
+
